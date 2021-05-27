f4bc12ad568b3fd784d7d40718746aa65359b4de
woshinibaba-hue <73807775+woshinibaba-hue@users.noreply.github.com>
2021-05-27 10:14:54 +0800

    Update request.js

--- src/netWork/request.js
+++ src/netWork/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { Loading } from 'element-ui'
 
 // 配置axios默认路径
-axios.defaults.baseURL = 'http://musicapi.leanapp.cn/'
+axios.defaults.baseURL = 'http://www.hjmin.com/'
 // 允许跨域请求携带cookie
 axios.defaults.withCredentials = true
 
